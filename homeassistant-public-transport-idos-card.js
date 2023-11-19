18b5f82003c4e9e725db1604aabcbbca58f47b65
Tomas Deingruber <Deingruber.Tomas@gmail.com>
2023-11-19 15:21:38 +0100

    Adds custom input entity UI with station hinting

--- homeassistant-public-transport-idos-card.js
+++ homeassistant-public-transport-idos-card.js
@@ -11,6 +11,7 @@ class IDOSCard extends LitElement {
     return {
       hass: {},
       config: {},
+      button_clicked: false,
     };
   }
 
@@ -27,30 +28,129 @@ class IDOSCard extends LitElement {
     text_arrival_config.entity = this.config.text_arrival_entity;
     text_arrival_config.name = "To";
 
+    var departure_station =
+      this.hass.states[this.config.text_departure_entity]?.state;
+    var arrival_station =
+      this.hass.states[this.config.text_arrival_entity]?.state;
+
     return html`
       <ha-card>
         <div class="card-content">
           <div class="station-selection-wrapper">
             <span class="station-selection">
-              <hui-entity-card
-                style="ha-card: {--ha-card-border-width: none;}"
-                .hass=${this.hass}
-                ._config=${text_departure_config}
-              ></hui-entity-card>
-              <hui-entity-card
-                .hass=${this.hass}
-                ._config=${text_arrival_config}
-              ></hui-entity-card>
+              <div class="station-selection-single-wrapper">
+                <div
+                  class="station-selection-single"
+                  @mousedown=${{
+                    handleEvent: (e) =>
+                      this.station_mousedown_event("station-from"),
+                    capture: true, // Needed to trigger before focus when clicking on input
+                  }}
+                  @click=${{
+                    handleEvent: (e) =>
+                      this.station_click_event("station-from"),
+                  }}
+                >
+                  <span class="station-selection-header">From</span>
+                  <input
+                    tabindex="1"
+                    id="station-from"
+                    class="station-selection-input"
+                    value=${departure_station}
+                    @focus=${{
+                      handleEvent: (e) => this.station_focus_event(e),
+                      capture: true,
+                    }}
+                    @blur=${{
+                      handleEvent: (e) => this.station_blur_event(e),
+                      capture: true,
+                    }}
+                    @input=${{
+                      handleEvent: (e) => this.station_input_event(e),
+                      capture: true,
+                    }}
+                  />
+                  <div class="hints-menu-wrapper">
+                    <div class="hints-menu">
+                      ${[...Array(5).keys()].map(() => {
+                        return html`
+                          <span
+                            class="hint"
+                            @mousedown=${{
+                              handleEvent: (e) =>
+                                this.station_hint_clicked(e, "station-from"),
+                              capture: true,
+                            }}
+                            >Item 1</span
+                          >
+                        `;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="station-selection-single-wrapper">
+                <div
+                  class="station-selection-single"
+                  @mousedown=${{
+                    handleEvent: (e) =>
+                      this.station_mousedown_event("station-to"),
+                    capture: true, // Needed to trigger before focus when clicking on input
+                  }}
+                  @click=${{
+                    handleEvent: (e) => this.station_click_event("station-to"),
+                  }}
+                >
+                  <span class="station-selection-header">To</span>
+                  <input
+                    tabindex="2"
+                    id="station-to"
+                    class="station-selection-input"
+                    value=${arrival_station}
+                    @focus=${{
+                      handleEvent: (e) => this.station_focus_event(e),
+                      capture: true,
+                    }}
+                    @blur=${{
+                      handleEvent: (e) => this.station_blur_event(e),
+                      capture: true,
+                    }}
+                    @input=${{
+                      handleEvent: (e) => this.station_input_event(e),
+                      capture: true,
+                    }}
+                  />
+                  <div class="hints-menu-wrapper">
+                    <div class="hints-menu">
+                      ${[...Array(5).keys()].map(() => {
+                        return html`
+                          <span
+                            class="hint"
+                            @mousedown=${{
+                              handleEvent: (e) =>
+                                this.station_hint_clicked(e, "station-to"),
+                              capture: true,
+                            }}
+                            >Item 1</span
+                          >
+                        `;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </span>
           </div>
           ${!this.config
-            ? ""
+            ? html` ${this.#_showWarning("Config not set in this card")} `
             : this.config.entities.map((ent) => {
                 const stateObj = this.hass.states[ent];
 
                 if (!stateObj) {
                   return html`
-                    ${this.#_showError("Unknown entity '" + ent + "'")}
+                    <div class="connection">
+                      ${this.#_showWarning("Unknown entity '" + ent + "'")}
+                    </div>
                   `;
                 }
 
@@ -58,8 +158,8 @@ class IDOSCard extends LitElement {
                 var delay;
 
                 if (!attributes.connections) {
-                  return html`<div class="not-found-station">
-                    Station not found.
+                  return html` <div class="connection">
+                    ${this.#_showWarning("Station not found")}
                   </div>`;
                 }
 
@@ -122,10 +222,18 @@ class IDOSCard extends LitElement {
 
   static get styles() {
     return css`
+      :host {
+        font-family: var(--paper-font-body1_-_font-family);
+      }
+
       .card-content {
         --card-background-color-darken: rgb(
           from var(--card-background-color) calc(r * 0.85) calc(g * 0.85)
             calc(b * 0.85)
+        );
+        --card-background-color-lighten: rgb(
+          from var(--card-background-color) calc(r * 1.15) calc(g * 1.15)
+            calc(b * 1.15)
         );
         margin: 0em;
         padding: 1.5em 0 0;
@@ -154,6 +262,72 @@ class IDOSCard extends LitElement {
         display: grid;
         grid-template-columns: repeat(2, minmax(0, 1fr));
         column-gap: 2.5%;
+      }
+
+      .station-selection-single-wrapper {
+        padding: 8px 16px 16px;
+        position: relative;
+      }
+
+      .station-selection-single {
+        padding: 1px; /* Fixes 1px input cursor blinking on borders*/
+        line-height: 28px;
+        display: flex;
+        flex-direction: column;
+      }
+
+      .station-selection-single:hover {
+        cursor: pointer;
+      }
+
+      .station-selection-header {
+        color: var(--secondary-text-color);
+        font-size: 16px;
+        font-weight: 500;
+      }
+
+      .station-selection-input {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-size: 28px;
+        font-family: inherit;
+        color: var(--primary-text-color);
+        background-color: inherit;
+        /* background-color: var(--card-background-color-darken); */
+        border: unset;
+        cursor: inherit;
+      }
+
+      .station-selection-input:focus {
+        outline: none;
+      }
+
+      .station-selection-input:focus + .hints-menu-wrapper {
+        display: block;
+      }
+
+      .hints-menu-wrapper {
+        display: none;
+        position: relative;
+      }
+
+      .hints-menu {
+        position: absolute;
+        z-index: 10;
+        box-sizing: border-box;
+        width: 100%;
+        background: var(--card-background-color-darken);
+      }
+
+      .hint {
+        padding: 8px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: block;
+      }
+
+      .hint:hover {
+        background-color: var(--card-background-color-lighten);
       }
 
       .connection,
@@ -258,6 +432,88 @@ class IDOSCard extends LitElement {
   // distribute all cards over the available columns.
   getCardSize() {
     return this.config.entities.length + 1;
+  }
+
+  station_mousedown_event(input_id_to_focus) {
+    var input_element = this.shadowRoot.getElementById(input_id_to_focus);
+    // Create a new 'change' event
+    var event = new Event("input");
+    // Dispatch it.
+    input_element.dispatchEvent(event);
+  }
+
+  // Focuses the input element by id
+  station_click_event(input_id_to_focus) {
+    var input_element = this.shadowRoot.getElementById(input_id_to_focus);
+    input_element.focus();
+    input_element.select();
+  }
+
+  // Show/hide hints (and fire hass value changed on focus out)
+  station_focus_event(event) {
+    this.button_clicked = "focus: " + event.target.value;
+  }
+
+  // Update text_entity in hass once we loose focus
+  station_blur_event(event, text_entity_in_hass) {
+    this.button_clicked = "blur: " + event.target.value;
+  }
+
+  // Update hints when text changes
+  station_input_event(event, station_type) {
+    var input_element = event.target;
+    var hints = input_element.parentElement.querySelectorAll(".hint");
+
+    // Hide hints while querying data
+    hints.forEach((hint) => {
+      hint.style.display = "none";
+    });
+
+    this.hass.connection
+      .sendMessagePromise({
+        type: "idos/search_stations",
+        station: input_element.value,
+        count: "10",
+      })
+      .then(
+        (resp) => {
+          hints.forEach((hint, index) => {
+            if (index < resp.stations?.length) {
+              hint.textContent = resp.stations[index];
+              hint.style.display = "block";
+            } else {
+              hint.textContent = "";
+              hint.style.display = "none";
+            }
+          });
+        },
+        (err) => {
+          console.error("Message failed!", err);
+        }
+      );
+  }
+
+  station_hint_clicked(event, input_id_to_update) {
+    this.shadowRoot.getElementById(input_id_to_update).value =
+      event.target.textContent;
+
+    var text_entity_id;
+    if (input_id_to_update === "station-from") {
+      text_entity_id = this.config.text_departure_entity;
+    } else if (input_id_to_update === "station-to") {
+      text_entity_id = this.config.text_arrival_entity;
+    }
+
+    if (text_entity_id) {
+      this.hass.callService("text", "set_value", {
+        value: event.target.textContent,
+        entity_id: text_entity_id,
+      });
+    }
+  }
+
+  #_showWarning(warning) {
+    return html` <hui-warning>${warning}</hui-warning> `;
   }
 
   #_showError(error) {
